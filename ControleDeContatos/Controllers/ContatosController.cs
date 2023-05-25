55f740ff9d7b161c0bda9ebb0d306827ffcfaea3
Lucas Mendes <lucasespassini@gmail.com>
2023-05-24 23:12:06 -0300

    Adicionar arquivos de projeto.

---
+++ ControleDeContatos/Controllers/ContatosController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ControleDeContatos.Controllers
+{
+    public class ContatosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public IActionResult Update()
+        {
+            return View();
+        }
+
+        public IActionResult Delete()
+        {
+            return View();
+        }
+
+        public IActionResult ConfirmDelete()
+        {
+            return View();
+        }
+    }
+}
---
+++ ControleDeContatos/Data/BancoContext.cs
@@ -0,0 +1,17 @@
+﻿using ControleDeContatos.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ControleDeContatos.Data
+{
+    public class BancoContext : DbContext
+    {
+        public BancoContext(DbContextOptions<BancoContext> dbOptions) : base(dbOptions) { }
+
+        public DbSet<ContatoModel> Contatos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql(new MySqlServerVersion(new Version(8, 0, 32)));
+        }
+    }
+}
